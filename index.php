1cab09aa620534e69a5a1aa72927523a0fa0a39d
AlessioCannas <cannas.alessio@alice.it>
2015-04-07 10:30:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
 ?>
 
 <?php
-	$conn= mysql_connect("localhost","root","");   
-        //$conn= mysql_connect("localhost","cannasAlessio","volpe795");
+	//$conn= mysql_connect("localhost","root","");   
+        $conn= mysql_connect("localhost","cannasAlessio","volpe795");
 	if(!$conn)
 	{
 		die("connessione FALLITA".mysql_error());
 	}
 	
 
-	$db= mysql_select_db("amm_alessio",$conn); 
-        //$db= mysql_select_db("amm14_cannasAlessio",$conn);
+	//$db= mysql_select_db("amm_alessio",$conn); 
+        $db= mysql_select_db("amm14_cannasAlessio",$conn);
 	if(!$conn)
 	{
 		die("connessione FALLITA ad amm_alessio".mysql_error());
