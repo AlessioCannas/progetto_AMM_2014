478315077de3c667ef5ba4077892bf5614ffab5a
AlessioCannas <cannas.alessio@alice.it>
2015-04-07 10:25:45 +0200

    Update connetti.php

--- connetti.php
+++ connetti.php
@@ -1,1 +1,24 @@
+<?php
 
+function connetti ()
+{
+        //$conn= mysql_connect("localhost","root","");   
+        conn= mysql_connect("localhost","cannasAlessio","volpe795");
+	if(!$conn)
+	{
+		die("connessione FALLITA".mysql_error());
+	}
+	
+
+	//$db= mysql_select_db("amm_alessio",$conn); 
+  $db= mysql_select_db("amm14_cannasAlessio",$conn);
+	
+	if(!$conn)
+	{
+		die("connessione FALLITA ad amm_alessio".mysql_error());
+	}
+        
+        return $conn;
+}
+
+?>
