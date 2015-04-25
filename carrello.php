e9d3c9e9c6c880d387bfced6656d48fc5c6cda9c
AlessioCannas <cannas.alessio@alice.it>
2015-04-25 19:49:56 +0200

    Update carrello.php

--- carrello.php
+++ carrello.php
@@ -10,7 +10,7 @@ function aggiungi ( $titolo, $prezzo, $quantita )
     {
         $_SESSION['carrello'][] = $prova;
 
-        echo "<h1>ho fatto le opererazioni richieste</h1>";
+        echo "<h1>$prova['titolo'] aggiunto al carrello</h1>";
     } 
 
 }
